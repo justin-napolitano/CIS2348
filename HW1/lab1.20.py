738a01f4775d9b8ac720df0426d8fd1506a3230e
Becky Tseng <112363045+Btseng22@users.noreply.github.com>
2022-09-19 12:45:00 -0500

    Update

---
+++ HW1/lab1.20.py
@@ -0,0 +1,13 @@
+user_num = int(input('Enter integer:\n'))
+
+print('You entered:', user_num)
+squared_num = user_num * user_num
+print(user_num, 'squared is', squared_num)
+cubed = user_num * user_num * user_num
+print('And', user_num, 'cubed is', cubed, '!!')
+
+user_num2 = int(input('Enter another integer:\n'))
+addition = user_num + user_num2
+multiplication = user_num * user_num2
+print(user_num, '+', user_num2, 'is', addition)
+print(user_num, '*', user_num2, 'is', multiplication)
