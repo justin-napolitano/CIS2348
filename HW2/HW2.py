554f4c5863fa46a5f59cf7c528c8e95fd9005b58
Becky Tseng <112363045+Btseng22@users.noreply.github.com>
2022-10-10 13:14:36 -0500

    HW2

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -2,22 +2,19 @@ import re
 from datetime import datetime
 
 dates = []
+with open("inputDates.txt", "r") as dates_file:
+    for date in dates_file:
+        correct_date = re.findall(r"\w+ \d{1,2}, \d{4}", date)
+        dates.extend(correct_date)
 
-while True:
-    date = input()
-    correct_date = re.findall(r"\w+ \d{1,2}, \d{4}", date)
-    dates.extend(correct_date)
+current_date = datetime.today().strftime('%Y-%m-%d')
 
-    if date == "-1":
-        break
+for item in dates:
+    date = datetime.strptime(item, '%B %d, %Y')
+    date_ = date.strftime('%Y-%m-%d')
 
-    current_date = datetime.today().strftime('%Y-%m-%d')
+    if date_ < current_date:
+        f = open("parsedDates.txt", "a")
+        f.write(date.strftime('%m/%d/%Y') + "\n")
 
-    for day in dates:
-        date = datetime.strptime(day, '%B %d, %Y')
-        date_format = date.strftime('%Y-%m-%d')
-
-    if date_format <= current_date:
-        print(date.strftime('%m/%d/%Y'))
-    elif date_format > current_date:
-        continue
+f.close()
